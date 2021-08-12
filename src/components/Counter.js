0d7a8b2c517749feabe8e19658b76d5580a33a7d
Paige <72361304+pckellyco@users.noreply.github.com>
2021-08-11 18:03:59 -0600

    Added Counter component

---
+++ src/components/Counter.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+
+export default function Counter() {
+    <div>
+        <button>+</button>
+        <p>Count: </p>
+        <button>-</button>
+    </div>
+}
